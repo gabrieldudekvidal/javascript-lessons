eb93470c3ed0b921b97dcb2f8780001d17020ee2
Gabriel Vidal <gabrieldkvl@outlook.com>
2023-11-16 14:11:31 -0800

    Lessons 10 and 11 completed

--- 10 - Calculator Exercise/main.js
+++ 10 - Calculator Exercise/main.js
@@ -1,22 +1,20 @@
-/* Named function */
+function calc() {
+    // Get the data based on id and convert to integer
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = document.querySelector("#operator").value;
+    // Declare the variable to add values afterwards
+    var calculate;
 
-function testExample(myName) {
-    var greeting = "Hi! my name is " + myName;
-    return greeting;
+    if (op == "add") {
+        calculate = a + b;
+    } else if (op == "min") {
+        calculate = a - b;
+    } else if (op == "div") {
+        calculate = a / b;
+    } else if (op == "mul") {
+        calculate = a * b;
+    } 
+    // Get using id and insert into html to show the result
+    document.querySelector("#result").innerHTML = calculate;
 }
-console.log(testExample("Gab"));
-
-/* Anonymous function */
-
-var testAnonymous = function(myNamename) {
-    var greeting = "Hi! my name is " + myNamename;
-    return greeting;
-}
-myName = "Gabriel"
-console.log(testAnonymous(myName));
-
-/* Immediatelly invoked function expression - When the page is loaded the function will be auto loaded */
-
-(function() {
-    alert("Hello!");
-}())
