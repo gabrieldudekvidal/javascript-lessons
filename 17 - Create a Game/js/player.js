44c1419bae718fb805bc448d10cc4d2668c23c25
Gabriel Vidal <gabrieldkvl@outlook.com>
2023-12-14 20:50:50 -0800

    Making a game part 2

---
+++ 17 - Create a Game/js/player.js
@@ -0,0 +1,12 @@
+// Declare in global scope
+let player;
+
+function Player(classType, health, mana, strength, agility, speed) {
+    // Properties
+    this.classType = classType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
